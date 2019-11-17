7552133663146b412d974d5bc6ce462df0087782
Dmitry <dim139@mail.ru>
2019-11-17 03:06:34 +0300

    Starting commit

---
+++ passenger.rb
@@ -0,0 +1,49 @@
+# Ассоциативный массив в который внесены данные о 3-х пассажирах
+passengers = [
+    {
+        ticket_number: 'РМ2010398 050298',
+        first_name: 'Венедикт',
+        second_name: 'Васильевич',
+        last_name: 'Ерофеев',
+        passport_number: '45 99 505281',
+        departure_city: 'Москва',
+        destination_city: 'Петушки'
+    },
+    {
+        ticket_number: 'РМ2010399 050298',
+        first_name: 'Иннокентий',
+        second_name: 'Петрович',
+        last_name: 'Шниперсон',
+        passport_number: '46 01 192872',
+        departure_city: 'Павловский Посад',
+        destination_city: 'Орехово-Зуево'
+    },
+    {
+        ticket_number: 'РМ2010399 050298',
+        first_name: 'Иван',
+        second_name: 'Васильевич',
+        last_name: 'Бунша',
+        passport_number: '47 33 912876',
+        departure_city: 'Москва',
+        destination_city: 'Владимир'
+    }
+]
+
+puts 'Пассажиры поезда Москва — Петушки'
+puts
+
+# Для каждого пассажира в массиве распечатаем его билет, воспользовавшись
+# методом массива each_with_index
+# number - номер места (+1 - сдвиг при полном прохождении на 1)
+passengers.each_with_index do |passenger, number|
+  puts "* * * Место № #{number + 1} * * *"
+  puts "Билет №  #{passenger[:ticket_number]}"
+  puts "Маршрут: #{passenger[:departure_city]} -> "\
+       "#{passenger[:destination_city]}"
+  puts "Пассажир: #{passenger[:first_name]}"\
+        # [0] выведет первый символ хранящийся в second_name
+       " #{passenger[:second_name][0]}."\
+       " #{passenger[:last_name]}"
+  puts "Паспорт: #{passenger[:passport_number]}"
+  puts
+end
